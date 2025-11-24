9e2c2a72bb537cc4e52249da6b1477c5c7a8c599
aori___ <hilariomarkryan3@gmail.com>
2025-11-24 16:55:22 +0800

    Refactor database connection to use static PDO instance for improved performance

--- db.php
+++ db.php
@@ -1,14 +1,18 @@
 <?php
 
 function execute($query) {
-    $hostname = $_ENV['DB_HOST'];
-    $username = $_ENV['DB_USER'];
-    $password = $_ENV['DB_PASS'];
-    $database = $_ENV['DB_NAME'];
+    static $pdo = null;
 
-    $dsn = "mysql:host=$hostname;dbname=$database";
-    $pdo = new PDO($dsn, $username, $password);
-    $pdo->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE, PDO::FETCH_OBJ);
+    if ($pdo === null) {
+        $hostname = $_ENV['DB_HOST'];
+        $username = $_ENV['DB_USER'];
+        $password = $_ENV['DB_PASS'];
+        $database = $_ENV['DB_NAME'];
+
+        $dsn = "mysql:host=$hostname;dbname=$database";
+        $pdo = new PDO($dsn, $username, $password);
+        $pdo->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE, PDO::FETCH_OBJ);
+    }
 
     return $pdo->query($query);
 }
