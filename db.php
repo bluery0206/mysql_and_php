b970b49832d229c2403621dedab65de2149d4cef
aori___ <hilariomarkryan3@gmail.com>
2025-11-26 11:56:50 +0800

    Refactor database connection to use hardcoded credentials and update index.php structure

--- db.php
+++ db.php
@@ -4,12 +4,14 @@ function connect() {
     static $pdo = null;
 
     if ($pdo === null) {
-        $hostname = $_ENV['DB_HOST'];
-        $username = $_ENV['DB_USER'];
-        $password = $_ENV['DB_PASS'];
-        $database = $_ENV['DB_NAME'];
+        // User Credentials
+        $hostname = "localhost";
+        $username = "root";
+        $password = "";
+        $database = "boilerplate";
 
         $dsn = "mysql:host=$hostname;dbname=$database";
+        
         $pdo = new PDO($dsn, $username, $password);
         $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
     }
--- index.php
+++ index.php
@@ -1,35 +1,56 @@
 <?php
 
-include __DIR__ . '/bootstrap.php';
 include __DIR__ . '/db.php';
 
 ?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>MySQL + PHP</title>
+</head>
+<body>
 
-<div>
-    <h2>Fetching a single row</h2>
 
-    <?php 
-        $query = "SELECT * FROM users WHERE username = ?";
-        $values = ['aori'];
-        $user = execute($query, $values)->fetch();
-    ?>
 
-    <h3><?= $user['username'] ?></h3>
+    <div>
+        <h2>Fetching a single row</h2>
 
-</div>
+        <?php 
+            $query = "SELECT * FROM users WHERE username = ?";
+            $values = ['aori'];
+            $user = execute($query, $values)->fetch();
+        ?>
 
-<div>
-    <h2>Fetching multiple rows</h2>
+        <h3><?= $user['username'] ?></h3>
 
-    <?php 
-        $query = "SELECT * FROM users WHERE isAdmin = ?";
-        $values = ['0'];
-        $user_list = execute($query, $values)->fetchAll();
-    ?>
+    </div>
 
-    <ul>
-        <?php foreach ($user_list as $user): ?>
-            <li><?= $user['username'] ?></li>
-        <?php endforeach?>
-    </ul>
-</div>
+
+
+    <hr>
+
+
+
+    <div>
+        <h2>Fetching multiple rows</h2>
+
+        <?php 
+            $query = "SELECT * FROM users WHERE isAdmin = ?";
+            $values = ['0'];
+            $user_list = execute($query, $values)->fetchAll();
+        ?>
+
+        <ul>
+            <?php foreach ($user_list as $user): ?>
+                <li><?= $user['username'] ?></li>
+            <?php endforeach ?>
+        </ul>
+    </div>
+    
+    
+    
+    
+</body>
+</html>
